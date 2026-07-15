17499eaac809098223923eec48362f08a7e1b58b
Antony Santero <santony85@users.noreply.github.com>
2026-07-15 14:54:09 +0200

    Change ALSA device configuration value

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const VIDEOS_DIR = path.join(__dirname, 'videos');
 if (!fs.existsSync(VIDEOS_DIR)) fs.mkdirSync(VIDEOS_DIR);
 
 const CONFIG = {
-  device: process.env.ALSA_DEVICE || 'plughw:3,0',
+  device: process.env.ALSA_DEVICE || 'hw:1,0',
   sampleRate: 44100,
   chunkMs: 200,
   weighting: 'A',
